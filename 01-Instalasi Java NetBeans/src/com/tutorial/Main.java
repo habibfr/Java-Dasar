9ee7aa5e6972e16af39b28851dc9f469e12ab37e
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2021-12-25 12:06:26 +0700

    commit terhadap Folder

---
+++ 01-Instalasi Java NetBeans/src/com/tutorial/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tutorial;
+
+/**
+ *
+ * @author Habib Fatkhul Rohman
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Selamat Datang di Kelas terbuka");
+        System.out.println("saya Habib Fatkhul Rohman dari Universitas Dinamika");
+        System.out.println("prodi S1 Sistem Informasi Siap Mengikuti Kuliah Online");
+        
+    }
+    
+}
